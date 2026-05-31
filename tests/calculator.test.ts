d6c569d39d2c70a9f036452f70e259dc35f050dd
Pablo Reyes <pjr.reyes@yavirac.edu.ec>
2026-05-31 15:03:15 -0500

    Fix tests and build

--- tests/calculator.test.ts
+++ tests/calculator.test.ts
@@ -1,8 +1,11 @@
-import test = require("node:test");
 import { Calculator } from "../src/calculator";
 
-const calc = new Calculator();
+describe("Calculator", () => {
 
-test("should add numbers correctly", () => {
-  expect(calc.add(2, 2)).toBe(5);
+  const calc = new Calculator();
+
+  test("should add numbers correctly", () => {
+    expect(calc.add(2, 2)).toBe(4);
+  });
+
 });
